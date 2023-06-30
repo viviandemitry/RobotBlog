f6039c8f0b0bceb7e7df1c0454adc0e9317e8ebf
Vivian De Mitry <vivian090@gmail.com>
2023-06-30 09:26:51 -0300

    project config

---
+++ src/storage/slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+};
+
+const sliceReducer = createSlice ({
+    name:"rootReducer",
+    initialState,
+    reducers: {
+
+    }
+})
+
+export const {} = sliceReducer.actions;
+
+export default sliceReducer.reducer;
