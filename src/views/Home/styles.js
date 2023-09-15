4f38fbbacb92003110bccd1728088e5e47b34071
Vivian De Mitry <vivian090@gmail.com>
2023-09-15 11:24:19 -0300

    conneted Api

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -6,4 +6,12 @@ export const ContainerMain = styled.div`
 export const NewsContainer = styled.div`
 
 `
+export const NewsContent = styled.a`
 
+`
+export const NewsImg = styled.img`
+
+`
+export const NewsTitle = styled.h2`
+
+`
--- src/views/Home/view.jsx
+++ src/views/Home/view.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from '../../components/Header'
 import * as Styles from './styles';
 
 export default function Home() {
+
+  const [data, setData] = useState([]);
+
+  async function getInfo(){
+    const response = await fetch('http://localhost:3002')
+    const dataApi = await response.json()
+    setData(dataApi)
+  }
+
+  useEffect(() => {
+    getInfo()
+  },[])
+
   return (
     <Styles.ContainerMain>
     <Header />
+    
     <Styles.NewsContainer>
-      
+      {
+        data.map((news) => (
+          <Styles.NewsContent href={news.link}>
+            <Styles.NewsImg src={news.imgUrl}/>
+            <Styles.NewsTitle>
+              {news.title}
+            </Styles.NewsTitle>
+          </Styles.NewsContent>   
+        ))
+      }
     </Styles.NewsContainer>
     </Styles.ContainerMain>
   )
